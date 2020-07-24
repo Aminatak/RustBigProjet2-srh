4b8aded8b2f32407b8ab28c25fd11f8fa4858d88
Yvan Sraka <yvan@sraka.xyz>
2020-07-24 19:35:43 +0200

    Feedbacks

--- src/shell/parser/commands/mod.rs
+++ src/shell/parser/commands/mod.rs
@@ -1,34 +1,37 @@
 use std::fmt;
 /// Init struct to model one Command
 #[derive(Clone, Debug)]
-pub struct Command{
+pub struct Command {
     pub command_name: String,
-    pub command_args: Vec<String>
+    pub command_args: Vec<String>,
 }
 
 impl fmt::Display for Command {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut args : String = String::from("");
+        let mut args: String = String::from("");
         for i in 0..self.command_args.len() {
             args.push_str(&self.command_args[i])
-        };
+        }
         write!(f, "{}   {}", self.command_name, args)
     }
-} 
+}
 
- impl Command{
-
+impl Command {
     /// Function make build command.
-    pub fn new(name : String, args: Vec<String>) -> Command {
-        Command{ command_name: name,command_args: args}
-    }
-
-    pub fn run_command (&self){
-        let status = std::process::Command::new(self.command_name.clone()).args(&self.command_args).status().expect("failed to execute command");         
-        if !status.success(){
-                println!("failed to execute");
+    pub fn new(name: String, args: Vec<String>) -> Command {
+        Command {
+            command_name: name,
+            command_args: args,
         }
     }
 
-
+    pub fn run_command(&self) {
+        let status = std::process::Command::new(self.command_name.clone())
+            .args(&self.command_args)
+            .status()
+            .expect("failed to execute command");
+        if !status.success() {
+            println!("failed to execute");
+        }
+    }
 }
--- src/shell/parser/mod.rs
+++ src/shell/parser/mod.rs
@@ -1,29 +1,29 @@
 pub mod commands;
 use commands::Command;
 
-pub fn parse_command(cmd : &mut String) -> Command{
+pub fn parse_command(cmd: &mut String) -> Command {
     let line = cmd.split_whitespace();
-    let mut tokens : Vec<String> = Vec::new();
+    let mut tokens: Vec<String> = Vec::new();
     for s in line {
         tokens.push(s.to_string());
     }
-    let command_name : String = tokens[0].clone();
-    let mut command_args : Vec<String> = Vec::new();
-    for i in 1..tokens.len() {           
+    let command_name: String = tokens[0].clone();
+    let mut command_args: Vec<String> = Vec::new();
+    for i in 1..tokens.len() {
         command_args.push(tokens[i].clone());
     }
     Command::new(command_name, command_args)
 }
 
-pub fn parse_piped_command_line(command_line : &mut String) -> Vec<Command>{
+pub fn parse_piped_command_line(command_line: &mut String) -> Vec<Command> {
     let line_splited = command_line.trim().split("|");
-    let mut cmds_string : Vec<String> = Vec::new();
+    let mut cmds_string: Vec<String> = Vec::new();
     for s in line_splited {
         cmds_string.push(s.to_string());
     }
 
-    let mut cmds_vector : Vec<Command> = Vec::new();    
-    for i in 0..cmds_string.len() {           
+    let mut cmds_vector: Vec<Command> = Vec::new();
+    for i in 0..cmds_string.len() {
         cmds_vector.push(parse_command(&mut cmds_string[i].clone()));
     }
     cmds_vector
